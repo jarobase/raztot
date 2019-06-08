674e26388000542f662974c6256f0514bc3ba698
Ben Busby <ben.busby7@gmail.com>
2019-06-08 13:48:20 -0600

    Added basic touch controls for raztot movement on mobile

--- app/static/js/socket-data.js
+++ app/static/js/socket-data.js
@@ -1,21 +1,98 @@
 $(document).ready(function () {
+    var isMoving = false;
+
     // Connect to the socket server
     var socket = io.connect('https://' + document.domain + ':' + location.port + '/raztot', { timeout: 5000 });
 
+    // Movement keycodes
+    const MOVE_LEFT = 37;
+    const MOVE_UP = 38;
+    const MOVE_RIGHT = 39;
+    const MOVE_DOWN = 40;
+
     socket.emit('poll');
 
-    document.onkeydown = function (event) {
+    // Socket commands for issuing pwm values to the servos
+    function stopMovement() {
+        socket.emit('move', null);
+    }
+
+    function startMovement(direction) {
         socket.emit('move', {
-            left: (event.keyCode || event.which) == "37" ? 1 : 0,
-            up: (event.keyCode || event.which) == "38" ? 1 : 0,
-            right: (event.keyCode || event.which) == "39" ? 1 : 0,
-            down: (event.keyCode || event.which) == "40" ? 1 : 0
-        });
+            left: direction == MOVE_LEFT ? 1 : 0,
+            up: direction == MOVE_UP ? 1 : 0,
+            right: direction == MOVE_RIGHT ? 1 : 0,
+            down: direction == MOVE_DOWN ? 1 : 0
+        }
+    }
+
+    // Keyboard controls
+    document.onkeydown = function (event) {
+        startMovement(event.keyCode);
     }
 
     document.onkeyup = function (event) {
-        socket.emit('move', null);
+        stopMovement();
     }
+
+    // Mobile controls
+    var sb = document.getElementById('swipe-box');
+    var xAxis = {
+        downX: 0,
+        upX: 0,
+        differenceX: 0
+    }
+    var yAxis = {
+        downY: 0,
+        upY: 0,
+        differenceY: 0
+    }
+
+    sb.addEventListener("touchstart", function(e){
+        xAxis.downX = e.clientX;
+        yAxis.downY = e.clientY;
+        isMoving = true;
+    });
+
+    document.ontouchend = function(e) {
+        isMoving = false;
+        stopMovement();
+    }
+
+    // Check for direction of user's swiping
+    sb.addEventListener("touchmove", function(e){
+        if (!isMoving) {
+            return;
+        }
+
+        xAxis.upX = e.clientX;
+        yAxis.upY = e.clientY;
+
+        var differenceX;
+        var differenceY;
+
+        differenceX = Math.abs(xAxis.downX - xAxis.upX);
+        differenceY = Math.abs(yAxis.downY - yAxis.upY);
+
+        // Check for swipe on X axis
+        if (differenceX > differenceY) {
+            if (xAxis.upX >= (xAxis.downX + 50)) {
+                startMovement(MOVE_RIGHT);
+            }
+
+            if (xAxis.upX <= (xAxis.downX - 50)) {
+                startMovement(MOVE_LEFT);
+            }
+        } else {
+            // Swipe is on Y axis
+            if (yAxis.upY >= (yAxis.downY + 30)) {
+                startMovement(MOVE_DOWN);
+            }
+            if (yAxis.upY <= (yAxis.downY - 30)) {
+                startMovement(MOVE_UP);
+            }
+        }
+    });
 
     // Receieve status messages through socket connection
     socket.on('status', function (msg) {
