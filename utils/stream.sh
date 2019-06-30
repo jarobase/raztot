a5d5ea708671157ce242f9e8f6fdbdf6d7081778
jnanar <jarobase@gmail.com>
2019-06-30 20:40:22 +0200

    Change stream settings to be compatible with my webcam

--- utils/stream.sh
+++ utils/stream.sh
@@ -3,7 +3,7 @@
 set -e
 function cleanup {
     echo "Stopping gstreamer..."
-    sudo pkill gst-launch-1.0
+     pkill ffmpeg
 }
 
 trap cleanup EXIT
@@ -11,7 +11,4 @@ trap cleanup EXIT
 # upstream command
 #gst-launch-1.0 -v autovideosrc  bitrate=3000000 keyframe-interval=20 ! video/x-h264, framerate=20/1 ! h264parse ! rtph264pay config-interval=1 pt=96 ! multiudpsink clients=127.0.0.1:8004,127.0.0.1:8005 
 
-# this one is working but too large packets
-# gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! ximagesink
-
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! videoconvert ! 'video/x-raw, width=320, height=240, framerate=30/1' ! rndbuffersize max=1316 min=1316 ! multiudpsink clients=127.0.0.1:8004,127.0.0.1:8005
+ffmpeg  -framerate 15/5 -i /dev/video0 -vf scale=320:240  -vcodec libx264 -y -f h264  -f rtp rtp://127.0.0.1:8004
